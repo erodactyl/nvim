4e78e3d669821768f12f8143d8379454460ea395
Erik Davtyan <erikdavtyan@gmail.com>
2023-11-04 12:04:28 +0400

    Stable

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -2,26 +2,32 @@ return {
   "EdenEast/nightfox.nvim",
   priority = 1000,
   config = function()
+    require('nightfox').setup {
+      -- options = {
+      --   transparent = true,
+      -- }
+    }
+
     vim.cmd.colorscheme 'nightfox'
   end
 }
 
--- {
+-- return {
 --   -- Theme inspired by Atom
 --   'navarasu/onedark.nvim',
 --   priority = 1000,
 --   config = function()
 --     vim.cmd.colorscheme 'onedark'
 --   end,
--- },
+-- }
 
--- {
+-- return {
 --   'sainnhe/gruvbox-material',
 --   priority = 1000,
 --   config = function()
 --     vim.g.gruvbox_material_better_performance = 1
 --     vim.g.gruvbox_material_background = 'hard'
---     -- vim.g.gruvbox_material_transparent_background = 1
+--     vim.g.gruvbox_material_transparent_background = 1
 --     vim.cmd.colorscheme 'gruvbox-material'
 --   end
--- },
+-- }
